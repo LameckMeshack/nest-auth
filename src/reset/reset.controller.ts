44d05b146f6f110cde4e72e14f638bfaff4747e2
Lameck Meshack <rhinolamer@gmail.com>
2023-04-17 15:34:50 +0300

    Sending email for resetting password

--- src/reset/reset.controller.ts
+++ src/reset/reset.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ResetService } from './reset.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller()
 
 export class ResetController {
     constructor(
-        private resetService: ResetService
+        private resetService: ResetService,
+        private mailerService: MailerService
     ) {
 
     }
@@ -17,8 +19,16 @@ export class ResetController {
             email, token
         })
 
+        const url = `http://localhost:3000/reset/${token}`;
+
+        await this.mailerService.sendMail({
+            to: email,
+            subject: 'Reset Password',
+            html: `<a href="${url}">Reset Password</a>`
+        })
+
         return {
-            message: "Success"
+            message: 'Check your email'
         }
     }
 }
--- src/reset/reset.module.ts
+++ src/reset/reset.module.ts
@@ -3,9 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reset } from './models/reset.entity';
 import { ResetController } from './reset.controller';
 import { ResetService } from './reset.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Reset])],
+    imports: [TypeOrmModule.forFeature([Reset]),
+    MailerModule.forRoot({
+        transport: {
+            host: 'localhost',
+            port: 1025,
+        },
+        defaults: {
+            from: 'no-reply@localhost.com',
+        },
+    })],
     controllers: [ResetController],
     providers: [ResetService]
 })
