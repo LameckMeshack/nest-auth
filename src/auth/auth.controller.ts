5a11ea6408330daeb018fdd3c128993abc329ff5
Lameck Meshack <rhinolamer@gmail.com>
2023-04-17 07:31:20 +0300

    Add http cookie with jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,22 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegUserDto } from './dto/register.dto';
 import * as bcrypt from 'bcryptjs';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
+import { PassThrough } from 'stream';
 
 
 
 @Controller()
 export class AuthController {
-    constructor(private authService: AuthService) { }
+    constructor(
+        private authService: AuthService,
+        private readonly jwtService: JwtService,
+    ) {
+        
+    }
 
     @Post('register')
     async register(@Body() user: RegUserDto) {
@@ -22,12 +30,14 @@ export class AuthController {
         // Replace the user's password with the hashed password
         user.password = hashedPassword;
         return this.authService.create(user);
-
     }
 
     //login
     @Post('login')
-    async login(@Body() loginDto: LoginDto) {
+    async login(
+        @Body() loginDto: LoginDto,
+        @Res({ passthrough: true }) response: Response,
+    ) {
         {
             const user = await this.authService.findOneBy({ email: loginDto.email });
 
@@ -38,6 +48,9 @@ export class AuthController {
             if (!await bcrypt.compare(loginDto.password, user.password)) {
                 throw new BadRequestException('Invalid credentials');
             }
+
+            const jwt = await this.jwtService.signAsync({ id: user.id });
+            response.cookie('jwt', jwt, { httpOnly: true });
             return user;
         }
     }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './models/user.entity';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]),
+  JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '60m' },
+  }),],
   controllers: [AuthController],
   providers: [AuthService]
 })
